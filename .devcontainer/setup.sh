775d1fe3e9a2e704091dc14e69010b2f34d775c2
Lee Stott <leestott@microsoft.com>
2024-08-07 10:29:51 +0000

    Update

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -6,10 +6,12 @@ sudo apt-get install git-lfs
 
 pip install -r ./.devcontainer/requirements.txt
 
+mkdir ./src/Models
+
 cd ./src/Models
 
 git lfs install
 
-git clone https://huggingface.co/microsoft/Phi-3-mini-128k-instruct
-
 git clone https://huggingface.co/microsoft/Phi-3-vision-128k-instruct-onnx-cpu
+
+git clone https://huggingface.co/microsoft/Phi-3-mini-128k-instruct-onnx

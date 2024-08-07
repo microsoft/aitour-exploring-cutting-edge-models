bb091c687865fd072f23c92dba1d559bc6b32be1
Kinfey <93169410+kinfey@users.noreply.github.com>
2024-08-07 10:01:55 +0000

    update init model for codespaces

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -5,3 +5,11 @@ sudo apt-get update
 sudo apt-get install git-lfs
 
 pip install -r ./.devcontainer/requirements.txt
+
+cd ./src/Models
+
+git lfs install
+
+git clone https://huggingface.co/microsoft/Phi-3-mini-128k-instruct
+
+git clone https://huggingface.co/microsoft/Phi-3-vision-128k-instruct-onnx-cpu
